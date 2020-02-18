29ff89b9a0a30ddb2a4c9f94b45a4bb0b12a3030
entattis15@itisvinci.com <entattis15@itisvinci.com>
2020-02-18 12:01:18 +0100

    start qtcreator configured with board's ip

---
+++ scripts/configureQtCreator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+IP="$(/home/stivvo/prog/msTest/scripts/./startPicocomScript.sh)"
+IP=${IP::-1}
+echo $IP
+
+CONFF="/home/stivvo/.config/QtProject/qtcreator/devices.xml"
+sed -i "s/192.168.0.1/${IP}/g" $CONFF
+#sed -i "s/^M//g" $CONFF
+vim $CONFF
+/home/stivvo/Qt/Tools/QtCreator/bin/./qtcreator.sh
+sed -i "s/${IP}/192.168.0.1/g" $CONFF
--- scripts/startPicocomScript.sh
+++ scripts/startPicocomScript.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cat "getIpBoard.sh" | /home/stivvo/prog/picocom/./picocom -qrx 1000 -b 115200 -r -l /dev/ttyUSB0 > file
-echo $(tail -n 2 file | head -n 1) > file
+echo $(tail -n 2 file | head -n 1)
+rm file
